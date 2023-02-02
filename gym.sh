c89c0ec5efefb95a661f98df18b2d9d7eb9a9787
wangshuo16 <wangshuo16@xiaomi.com>
2023-02-02 14:34:51 +0800

    fix bug when using crontab
    Signed-off-by: wangshuo16 <wangshuo16@xiaomi.com>

--- gym.sh
+++ gym.sh
@@ -1,17 +1,17 @@
 function Run() {
     time=`date +%Y%m%d%H%M`
+    cd $localdir
     mkdir -p ./log/
     /usr/bin/python3 ./gym_order.py > ./log/gym_order_${time}.log
 }
 
 function Install() {
-
-    localdir=`pwd`
+    cd $localdir && localdir=`pwd`
     echo "1. crontab -e"
-    echo "2. input \"55 10 * * sun,mon,tue,wed,thu /bin/bash ${localdir}/$0\""
+    echo "2. input \"55 10 * * sun,mon,tue,wed,thu /bin/bash ${localdir}/`basename $0`\""
 }
 
-
+localdir=`dirname $0`
 if [ "$#" -gt "0" ];then
     if [ "$1" = "-i" ];then
         Install
